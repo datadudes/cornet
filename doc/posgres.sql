8c0959d452e8b06e4d86de2e91a5af5bdf3c5a3d
Franklin M. Siler <me@franksiler.com>
2017-09-06 21:28:07 -0500

    adding postgres example schema

---
+++ doc/posgres.sql
@@ -0,0 +1,11 @@
+BEGIN;
+CREATE TABLE people (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name TEXT
+);
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY NOT NULL,
+    person INTEGER NOT NULL REFERENCES people(id),
+    address TEXT NOT NULL
+);
+COMMIT;
